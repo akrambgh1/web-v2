d3cef3f525ae97fac7e5783e6f289daeceb40969
akrambgh1 <121614726+akrambgh1@users.noreply.github.com>
2025-03-14 05:36:36 +0100

    af

---
+++ pages/profile-akram.jsx
@@ -0,0 +1,16 @@
+
+
+function ProfileAkram() {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center bg-blackp h-screen">
+              <div>
+                  <h1 className="text-4xl font-bold text-center text-gray-900 dark:text-gray-100
+                  ">Comming soon...</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default ProfileAkram;
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import ProfileAkram  from "../pages/profile-akram";
+
 
 export default function App() {
   return (
     <Router basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Benghezal-akram" element={<ProfileAkram />} />
+
       </Routes>
     </Router>
   );
